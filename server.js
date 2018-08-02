e83206748f3c1499581186ff6f5967802a26c3e0
Jaime Lopez <detec.azul@gmail.com>
2018-08-02 12:14:13 +0100

    Added CROSS access from any address

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let port = process.env.PORT || 8080;
 
 // allows cross origin resource sharing
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", *);
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();

8aee828b2c12262ab40fa621ad336e4a95e67552
Jaime Lopez <detec.azul@gmail.com>
2018-08-03 14:42:11 +0100

    Server can save the game and return the id to be added to the status of the game

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(publicPath));
 // allows cross origin resource sharing
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();

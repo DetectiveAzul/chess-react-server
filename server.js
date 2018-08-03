1ddd229a4510df5699e9b14de799462edcd953e3
Jaime Lopez <detec.azul@gmail.com>
2018-08-03 13:55:01 +0100

    Allowing all RESTful methods on CORS config

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.static(publicPath));
 
 // allows cross origin resource sharing
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();

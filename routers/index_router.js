0c3b0f4d08b009e3dcba18768deeb8e3d75143a7
Jaime Lopez <detec.azul@gmail.com>
2018-08-06 15:24:36 +0100

    Changed mongoDB url to online mLAB deploy

--- routers/index_router.js
+++ routers/index_router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const dbConfig = require('../config.js');
 const MongoClient = require('mongodb').MongoClient;
 const gamesRouter = require('./games_router.js');
 
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+MongoClient.connect(`${dbConfig.mongodbUrl}`, (err, client) => {
   console.log('Connected to DB');
   const db = client.db('chess');
   const gamesCollection = db.collection('games');

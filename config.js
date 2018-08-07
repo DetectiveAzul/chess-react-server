dd5b93a3e25f221feb19815d9defed83923d33d8
Jaime Lopez <detec.azul@gmail.com>
2018-08-07 09:18:47 +0100

    Added code for swapping from remote db to local db

--- config.js
+++ config.js
@@ -2,4 +2,8 @@ const config = {
   mongodbUrl: 'mongodb://chess-master:chess-master1@ds113692.mlab.com:13692/chess'
 }
 
+// const config = {
+//   mongodbUrl: 'mongodb://localhost:27017'
+// }
+
 module.exports = config;

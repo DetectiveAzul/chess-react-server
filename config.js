5cdd6a89b6102ec4f5db2924c4cfc8d12d4b6057
Jaime Lopez <detec.azul@gmail.com>
2018-09-10 09:24:54 +0100

    Modified mongodb db to online one

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// const config = {
-//   mongodbUrl: 'mongodb://chess-master:chess-master1@ds113692.mlab.com:13692/chess'
-// }
-
 const config = {
-  mongodbUrl: 'mongodb://localhost:27017'
+  mongodbUrl: 'mongodb://chess-master:chess-master1@ds113692.mlab.com:13692/chess'
 }
+
+// const config = {
+//   mongodbUrl: 'mongodb://localhost:27017'
+// }
 
 module.exports = config;
